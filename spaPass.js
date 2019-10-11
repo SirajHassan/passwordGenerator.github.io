0ecd254807733b5b6524bf23d67b7a577e924733
Siraj Hassan <sirajhassan@yahoo.com>
2019-10-10 22:17:32 -0500

    fist commit

---
+++ spaPass.js
@@ -0,0 +1,297 @@
+
+/////// letter arrays ///
+
+var words = ["the","of","to","and","a","in","is","it","you","that","he","was","for","on","are","with","as","I","his","they","be","at","one","have","this","from","or","had","by","hot","word","but","what","some","we","can","out","other","were","all","there","when","up","use","your","how","said","an","each","she","which","do","their","time","if","will","way","about","many","then","them","write","would","like","so","these","her","long","make","thing","see","him","two","has","look","more","day","could","go","come","did","number","sound","no","most","people","my","over","know","water","than","call","first","who","may","down","side","been","now","find","any","new","work","part","take","get","place","made","live","where","after","back","little","only","round","man","year","came","show","every","good","me","give","our","under","name","very","through","just","form","sentence","great","think","say","help","low","line","differ","turn","cause","much","mean","before","move","right","boy","old","too","same","tell","does","set","three","want","air","well","also","play","small","end","put","home","read","hand","port","large","spell","add","even","land","here","must","big","high","such","follow","act","why","ask","men","change","went","light","kind","off","need","house","picture","try","us","again","animal","point","mother","world","near","build","self","earth","father","head","stand","own","page","should","country","found","answer","school","grow","study","still","learn","plant","cover","food","sun","four","between","state","keep","eye","never","last","let","thought","city","tree","cross","farm","hard","start","might","story","saw","far","sea","draw","left","late","run","don't","while","press","close","night","real","life","few","north","open","seem","together","next","white","children","begin","got","walk","example","ease","paper","group","always","music","those","both","mark","often","letter","until","mile","river","car","feet","care","second","book","carry","took","science","eat","room","friend","began","idea","fish","mountain","stop","once","base","hear","horse","cut","sure","watch","color","face","wood","main","enough","plain","girl","usual","young","ready","above","ever","red","list","though","feel","talk","bird","soon","body","dog","family","direct","pose","leave","song","measure","door","product","black","short","numeral","class","wind","question","happen","complete","ship","area","half","rock","order","fire","south","problem","piece","told","knew","pass","since","top","whole","king","space","heard","best","hour","better","true","during","hundred","five","remember","step","early","hold","west","ground","interest","reach","fast","verb","sing","listen","six","table","travel","less","morning","ten","simple","several","vowel","toward","war","lay","against","pattern","slow","center","love","person","money","serve","appear","road","map","rain","rule","govern","pull","cold","notice","voice","unit","power","town","fine","certain","fly","fall","lead","cry","dark","machine","note","wait","plan","figure","star","box","noun","field","rest","correct","able","pound","done","beauty","drive","stood","contain","front","teach","week","final","gave"]
+
+// "green","oh","quick","develop","ocean","warm","free","minute","strong","special","mind","behind","clear","tail","produce","fact","street","inch","multiply","nothing","course","stay","wheel","full","force","blue","object","decide","surface","deep","moon","island","foot","system","busy","test","record","boat","common","gold","possible","plane","stead","dry","wonder","laugh","thousand","ago","ran","check","game","shape","equate","hot","miss","brought","heat","snow","tire","bring","yes","distant","fill","east","paint","language","among","grand","ball","yet","wave","drop","heart","am","present","heavy","dance","engine","position","arm","wide","sail","material","size","vary","settle","speak","weight","general","ice","matter","circle","pair","include","divide","syllable","felt","perhaps","pick","sudden","count","square","reason","length","represent","art","subject","region","energy","hunt","probable","bed","brother","egg","ride","cell","believe","fraction","forest","sit","race","window","store","summer","train","sleep","prove","lone","leg","exercise","wall","catch","mount","wish","sky","board","joy","winter","sat","written","wild","instrument","kept","glass","grass","cow","job","edge","sign","visit","past","soft","fun","bright","gas","weather","month","million","bear","finish","happy","hope","flower","clothe","strange","gone","jump","baby","eight","village","meet","root","buy","raise","solve","metal","whether","push","seven","paragraph","third","shall","held","hair","describe","cook","floor","either","result","burn","hill","safe","cat","century","consider","type","law","bit","coast","copy","phrase","silent","tall","sand","soil","roll","temperature","finger","industry","value","fight","lie","beat","excite","natural","view","sense","ear","else","quite","broke","case","middle","kill","son","lake","moment","scale","loud","spring","observe","child","straight","consonant","nation","dictionary","milk","speed","method","organ","pay","age","section","dress","cloud","surprise","quiet","stone","tiny","climb","cool","design","poor","lot","experiment","bottom","key","iron","single","stick","flat","twenty","skin","smile","crease",
+// "hole","trade","melody","trip","office","receive","row","mouth","exact","symbol","die","least","trouble","shout","except","wrote","seed","tone","join","suggest","clean","break","lady","yard","rise","bad","blow","oil","blood","touch","grew","cent","mix","team","wire","cost","lost","brown","wear","garden","equal","sent","choose","fell","fit","flow","fair","bank","collect","save","control","decimal","gentle","woman","captain","practice","separate","difficult","doctor","please","protect","noon","whose","locate","ring","character","insect","caught","period","indicate","radio","spoke","atom","human","history","effect","electric","expect","crop","modern","element","hit","student","corner","party","supply","bone","rail","imagine","provide","agree","thus","capital","won't","chair","danger","fruit","rich","thick","soldier","process","operate","guess","necessary","sharp","wing","create","neighbor","wash","bat","rather","crowd","corn","compare","poem","string","bell","depend","meat","rub","tube","famous","dollar","stream","fear","sight","thin","triangle","planet","hurry","chief","colony","clock","mine","tie","enter","major","fresh","search","send","yellow","gun","allow","print","dead","spot","desert","suit","current","lift","rose","continue","block","chart","hat","sell","success","company","subtract","event","particular","deal","swim","term","opposite","wife","shoe","shoulder","spread","arrange","camp","invent","cotton","born","determine","quart","nine","truck","noise","level","chance","gather","shop","stretch","throw","shine","property","column","molecule","select","wrong","gray","repeat","require","broad","prepare","salt","nose","plural","anger","claim","continent","oxygen","sugar","death","pretty","skill","women","season","solution","magnet","silver","thank","branch","match","suffix","especially","fig","afraid","huge","sister","steel","discuss","forward","similar","guide","experience","score","apple","bought","led","pitch","coat","mass","card","band","rope","slip","win","dream","evening","condition","feed","tool","total","basic","smell","valley","nor","double","seat","arrive","master","track","parent","shore","division","sheet","substance","favor","connect","post","spend","chord","fat","glad","original","share","station","dad","bread","charge","proper","bar","offer","segment","slave","duck","instant","market","degree","populate","chick","dear","enemy","reply","drink","occur","support","speech","nature","range","steam","motion","path","liquid","log","meant","quotient","teeth","shell","neck"];
+
+var dic = {"l":"1","i":"1","Z":"2","z":"2","E":"3","A":"4",
+"s":"5","S":"5","b":"6","B":"8","g":"9","o":"0","O":"0"};
+
+var leftSide = ["q","w","e","r","t","a","s","d","f","g","z","x","c","v"];
+var rightSide = ["y","u","i","o","p","h","j","k","l","b","n","m"];
+var subs = {"l":"1","i":"1","Z":"2","z":"2","E":"3","A":"4","s":"5","S":"5","b":"6","B":"8","g":"9","o":"0","O":"0"};
+var sizeTable = [];
+var maxInd = 0;
+
+
+///////////////////////////////
+
+
+function displayText(words) {
+  document.getElementById('data').innerHTML = words;
+}
+
+function getInputs(){
+    // get current values
+    var minWL = document.getElementById('MinWL').value;
+    var maxWL = document.getElementById('MaxWL').value;
+    var maxLength = document.getElementById('MaxLength').value;
+    //bools
+    var easyTyping = document.getElementById('EasyTyping').checked;
+    var numSub = document.getElementById('NumSub').checked;
+
+    // alert(numSub);
+    // alert(easyTyping);
+
+     // check inputs.. anything less to zero set to 0 or not integer
+     // ask to restart process..
+
+     arr = process(minWL,maxWL,maxLength,easyTyping,numSub,words);
+     return arr;
+
+
+  }
+
+// figure out whether a word is well balanced (true)..
+function balancedWord(word){
+  var leftSide = ["q","w","e","r","t","a","s","d","f","g","z","x","c","v"];
+  var rightSide = ["y","u","i","o","p","h","j","k","l","b","n","m"];
+
+  // if single letter
+  if (word.length <= 2){
+    return true;
+  }
+  else{
+    var wordArr = word.split('');
+  }
+
+  var L = 0;
+  var R = 0;
+  var length = word.length;
+
+  for( i = 0; i < length;i++){ //error here?
+    // alert('yello')
+    if ((leftSide.indexOf(wordArr[i])) >= 0){
+      L+=1;
+    }
+    else{
+      R+=1;
+    }
+  }
+  var rRat = R/length;
+  var lRat = L/length;
+  //test if left or right is bigger..
+  if( lRat > 0.75 || rRat > 0.75){
+    return(false);
+  }
+  else{
+    return(true);
+  }
+}
+
+//subsitute in numbers that look similair to letters in a word.
+// input dictionary of keys/valus
+// returns string version.
+
+function subLetters(word,dic){
+
+  // if single letter
+  if (word.length <=2){
+    return word;
+  }
+  else{
+    var wordArr = word.split('');
+  }
+
+
+  for( i = 0; i < wordArr.length;i++){
+    var value = dic[wordArr[i]];
+    if (typeof(value) != "undefined"){
+      wordArr[i] = value;
+    }
+  }
+  //alert(wordArr.join(''));
+  return wordArr.join('');
+}
+
+//check whether word is within max or min size
+// return false if out of bounds .
+function checkMaxMin(word,max,min){
+
+  var len = word.length;
+  if (len > max || len < min){
+    return(false);
+  }
+  else{
+    return(true);
+  }
+}
+
+function checkTotalLength(words,max){
+  //get total size of all words in array..
+  var sum = 0;
+  for( i = 0; i < words.length;i++){
+    sum += words[0].length;
+  }
+  if(sum > max){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+
+
+
+// take inputs and process into array that will be outputted
+// as html table..
+
+// take inputs and process into array that will be outputted
+// as html table..
+function getWords(minWL,maxWL,easyTyping,numSub,words){
+  // alert('yo');
+  // process word
+  var goodWords = [];
+
+  for( i = 0; i < words.length;i++){
+    var word = words[i];
+    //skip non worthy words
+    if(checkMaxMin(words[i],maxWL,minWL) == false){
+      // alert('hey')
+      continue;
+    }
+    if(easyTyping == true ){
+      if(balancedWord(words[i]) == false){
+        alert('hi')
+        continue;
+      }
+    }
+    if(numSub == true){
+      word = subLetters(words[i],dic);
+    }
+
+    goodWords.push(word); // add to array
+  }
+
+  return goodWords;
+}
+
+//take array of words, and fill in an array of
+//lines until line is at maxLength limit.
+//when at limit move onto the next line.
+function compileTableLine(words, maxLength){
+  var lineArr = [];
+  var size = 0;
+  var line = [];
+
+  for( i = 0; i < words.length;i++){
+
+    if (size + words[i].length <= maxLength){
+      line.push(words[i]);
+      size += words[i].length;
+    }
+    //reset
+    else{
+      // alert(line);
+
+      sizeTable.push([size])
+
+      // alert(sizeTable);
+      size = 0;
+
+
+      lineArr.push(line);
+      line = [];
+      continue;
+    }
+  }
+  return lineArr;
+
+}
+
+// return a line array from all inputs to be turned into table
+function process(minWL,maxWL,maxLength,easyTyping,numSub,words){
+  goodWords = getWords(minWL,maxWL,easyTyping,numSub,words);
+  return(compileTableLine(goodWords,maxLength));
+}
+
+
+
+//take javascript results array and display as html table
+//https://stackoverflow.com/questions/20407781/dynamically-generated-table-using-an-array-to-fill-in-td-values
+function display(lineArr){
+
+
+
+
+}
+
+function submit(){
+  // alert('submit');
+  var arr = getInputs();
+  //alert(arr);
+  GenerateTable(arr,'wordTable');
+  GenerateTable(sizeTable,'sizeTable')
+  //cleanTable();
+
+}
+
+function getLargestRowSize(array){
+  var largest = 0;
+  for (var i = 0; i < arr.length; i++){
+    var current = array[i].length;
+    if (current > largest){
+      largest = current;
+    }
+  }
+  return largest;
+}
+
+
+
+function GenerateTable(words,id) {
+
+
+        //Create a HTML Table element.
+        var table = document.createElement("TABLE");
+        table.border = "1";
+
+        //Get the count of columns.
+        var columnCount = getLargestRowSize(words);
+
+        //Add the header row.
+        var row = table.insertRow(-1);
+        for (var i = 0; i < columnCount; i++) {
+            var headerCell = document.createElement("TH");
+            //
+            //row.appendChild(headerCell);
+        }
+
+        //Add the data rows.
+        for (var i = 0; i < words.length; i++) {
+            row = table.insertRow(-1);
+            for (var j = 0; j < columnCount; j++) {
+                var cell = row.insertCell(-1);
+                if (words[i][j] != "undefined"){
+                  cell.innerHTML = words[i][j];
+                }
+                else{
+                  cell.innerHTML = '';
+                }
+            }
+        }
+
+        var dvTable = document.getElementById(id);
+        dvTable.innerHTML = "";
+        dvTable.appendChild(table);
+    }
+
+
+
+
+
+function cleanTable(){
+  var table = document.getElementById('wordTable');
+      if (table.rows.length != "undefined"){
+        var rowCount = table.rows.length;
+       }
+         for (var r = 0, n = table.rows.length; r < n; r++) {
+           for (var c = 0, m = table.rows[r].cells.length; c < m; c++) {
+            if((table.rows[r].cells[c].firstChild.value) == "undefined"){
+              table.rows[r].cells[c].firstChild.value = " ";
+            }
+        }
+    }
+}
